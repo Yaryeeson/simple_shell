773c48a6b3200f1788a22b90684b2e31b0681bf9
Yaryeeson <caryeeson@gmail.com>
2023-06-21 16:32:51 -0700

    command.c

--- command.c
+++ command.c
@@ -7,73 +7,73 @@
 #define MAX_COMMAND_LENGTH 100
 
 /**
-* Main - Entry point of simple shell
+* main - Entry point of simple shell
 * Description: Display the prompt, wait for the user to type the command
 * and execute the command
-* The shell support baqsic command execution without arguments
+* The shell support basic command execution without arguments
 * Return: 0
 */
-
-int main(void)
-
-char command[MAX_COMMAND_LENGTH];
-int status;
+int main()
 {
+	char command[MAX_COMMAND_LENGTH];
+	int status;
+	pid_t pid;
 
 	while (1)
 	{
-	printf("ky_shell?"); /* Display the prompt */
-	fflush(stdout);
+		printf("ky_shell$"); /* Display the prompt */
+		fflush(stdout);
 
-	if (fgets(command, sizeof(command), stdin) == NULL)
-	 break; /* Exit the loop if an error occurs or EOF is reached */
+		if (fgets(command, sizeof(command), stdin) == NULL)
+		{
+			break; /* Exit the loop if an error occurs or EOF is reached */
+		}
+		command[strcspn(command, "\n")] = '\0'; /* Remove the newline characters */
 
-	command[strcspn(command, "\n")] = '\0'; /* Remove the newline characters */
+		if (strcmp(command, "greet") == 0)
+		{
+			printf("Hello, how can we help you\n");
+			continue;
+		}
+		if (strcmp(command, "date") == 0)
+		{
+			system("date"); /* Execute the "date" command using system() */
+			continue; /* Skip the rest of the loop and display the prompt again */
+		}
 
-	if (strcmp(command, "greet") == 0)
-	{
-	printf("Hello, how can we help you?\n");
-	continue;
+		if (strcmp(command, "exit") == 0)
+		{
+			printf("Exiting the shell...\n");
+			return 0; /* Terminate the while loop and exit the shell */
+		}
+
+		pid = fork(); /* Fork a child process */
+
+		if (pid < 0)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		if (pid == 0)
+		{
+			/* Child process */
+			if (execlp(command, command, NULL) < 0)
+			{
+				printf("command not found: %s\n", command);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			/* Parent process */
+			/* Wait for the child process to complete */
+			if (waitpid(pid, &status, WUNTRACED) == -1)
+			{
+				perror("waitpid");
+				exit(EXIT_FAILURE);
+			}
 	}
-			
-	if (strcmp(command, "date") == 0)
-	{
-	system("date"); /* Execute the "date" command using system() */
-	continue; /* Skip the rest of the loop and display the prompt again */
-	}
-
-	if (strcmp(command, "exit") == 0)
-	{
-	printf("Exiting the shell...\n");
-	break; /* Terminate the while loop and exit the shell */
-	}
-
-	pid_t pid;
-	pid = fork(); /* Fork a child process */
-
-	if (pid < 0)
-	{
-	perror("fork");
-	exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-	/* Child process */
-
-	if (execlp(command, command, NULL) < 0)
-	{
-	printf("command not found: %s\n", command);
-	exit(EXIT_FAILURE);
-	}
-	}
-	else
-
-	/* Parent process */
-	/* Wait for the child process to complete int status */
-	if (waitpid(pid, &status, 3) == -1)
-	{
-	perror("waitpid");
-	exit(EXIT_FAILURE);
-	}
-	return (0);
+}
+/* Control should never reach here */
+return 0;
 }
